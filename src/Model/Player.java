2cb8f28a0fb48ab6321b4f15f3c1d43bc6711d68
paco <paco@paco-K55VD>
2015-03-28 12:14:49 +0100

    Commenting some methods. TODO: Test methods

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -12,6 +12,12 @@ import java.util.ArrayList;
  * @author paco
  */
 public class Player {
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //                                                                        //
+    //                               Attributes                               //
+    //                                                                        //    
+    ////////////////////////////////////////////////////////////////////////////
 
     private boolean dead;
     private String name;
@@ -23,10 +29,23 @@ public class Player {
     private ArrayList<Treasure> visibleTreasures;
     private BadConsequence pendingBadConsequence;
 
+    ////////////////////////////////////////////////////////////////////////////
+    //                                                                        //
+    //                             Private methods                            //
+    //                                                                        //    
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Revive the player
+     */
     private void bringToLive(){
         this.dead = false;
     }
     
+    /**
+     * Increment the player level
+     * @param levels Incremented levels
+     */
     private void incrementLevels(int levels){
         this.level += levels;
         if (this.level > 10){
@@ -34,6 +53,10 @@ public class Player {
         }
     }
     
+    /**
+     * Decrement the player level
+     * @param levels Decremented levels
+     */
     private void decrementLevels(int levels){
         this.level -= levels;
         if (this.level < 1){
@@ -41,30 +64,70 @@ public class Player {
         }
     }
     
+    /**
+     * BadConsequence setter
+     * @param badConsequence Bad consequence to be set 
+     */
     private void setPendingBadConsequence(BadConsequence badConsequence){
         this.pendingBadConsequence = badConsequence;
     }
     
+    /**
+     * The player dies
+     */
     private void die(){
         this.dead = true;
     }
     
+    /**
+     * Make the player discard necklace. 
+     */
     private void discardNecklaceIfVisible(){
-        
+        for (Treasure t: this.visibleTreasures){
+            if (t.getType() == TreasureKind.NECKLACE){
+                this.visibleTreasures.remove(t);
+            }
+        }
     }
     
+    /**
+     * Kill the player if he had no treasures. Player cannot stay alive w/out
+     * treasures, so he's killed and he will get new treasures when revive
+     */
     private void dieIfNoTreasures(){
         if(this.hiddenTreasures.isEmpty() && this.visibleTreasures.isEmpty()){
             this.dead = true;
         }
     }
     
+    /**
+     * Test if player can buy levels or not
+     * @param levels Number of levels the player wants to buy
+     * @return True if it's possible to buy them, false if not
+     */
     private boolean canIBuyLevels(int levels){
         return (this.level + levels < 10);
     }
     
+    ////////////////////////////////////////////////////////////////////////////
+    //                                                                        //
+    //                             Public methods                             //
+    //                                                                        //    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Computa el valor en monedas de oro de un array de tesoros que se pasa como
+     * argumento. Este valor se utiliza después para ver el número de niveles que
+     * puede comprar el jugador. Puede comprar un nivel por cada 1000 monedas de oro
+     * @param treasures Tesoros que quiere vender
+     * @return Valor en monedas de oro del array de tesoros
+     */
     protected float computeGoldCoinsValue(ArrayList<Treasure> treasures){
-        
+        float totalLevels = 0;
+        for (Treasure t: treasures){
+            totalLevels += t.getGoldCoins();
+        }
+        return totalLevels/1000;
     }
     
     public void applyPrize(Prize prize){
@@ -83,8 +146,27 @@ public class Player {
         
     }
     
+    /**
+     * Comprueba si el jugador reúne las condiciones necesarias para convertir en
+     * visible el tesoro que se pasa como argumento.
+     * @param treasure Tesoro que se quiere convertir en visible
+     * @return True si se puede convertir en visible, false en caso contrario
+     */
     public boolean canMakeTreasureVisible(Treasure treasure){
-        
+        TreasureKind treasureType = treasure.getType();
+        int count = 0;
+        for (Treasure t: this.visibleTreasures){
+            if (t.getType() == treasureType){
+                count+=1;
+            }
+        }
+        if (count < 1 || (count < 2 && treasureType == TreasureKind.ONEHAND)){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     
     public void DiscardVisibleTreasure(Treasure treasure){
@@ -99,12 +181,23 @@ public class Player {
         
     }
     
+    /**
+     * Getter para el nivel de combat del jugador. Este nivel se computa como el
+     * nivel real del jugador más los niveles que le otorguen los bonus de los 
+     * tesoros visibles que lleve.
+     * @return Nivel total de combate del jugador
+     */
     public int getCombatLevel(){
         
     }
     
+    /**
+     * Comprueba si el estado del jugador es consistente como para que el jugador
+     * termine su turno.
+     * @return True si el jugador está en un estado válido, false en caso contrario
+     */
     public boolean validState(){
-        return this.pendingBadConsequence == null &&
+        return this.pendingBadConsequence.isEmpty() &&
                 this.hiddenTreasures.size() <= MAXHIDDENTREASURES;
     }
     
@@ -112,24 +205,45 @@ public class Player {
         
     }
     
+    /**
+     * Método que comprueba si el jugador está muerto.
+     * @return True si lo está, false en caso contrario
+     */
     public boolean isDead(){
         return this.dead;
     }
     
+    /**
+     * Comprueba si el jugador tiene algún tesoro visible.
+     * @return True si lo tiene, false en caso contrario
+     */
     public boolean hasVisibleTreasures(){
         return !this.visibleTreasures.isEmpty();
     }
     
+    /**
+     * Constructor del jugador. Inicializa al jugador a un estado válido, con el
+     * que inicia la partida.
+     * @param name Nombre del jugador en la partida
+     */
     public Player(String name){
         this.name = name;
         this.level = 1;
         this.dead = true;
     }
     
+    /**
+     * Getter para el array de tesoros visibles. 
+     * @return Array de tesoros visibles
+     */
     public ArrayList<Treasure> getVisibleTreasures(){
         return this.visibleTreasures;
     }
     
+    /**
+     * Getter para el array de tesoros ocultos.
+     * @return Array de tesoros ocultos
+     */
     public ArrayList<Treasure> getHiddenTreasures(){
         return this.hiddenTreasures;
     }
